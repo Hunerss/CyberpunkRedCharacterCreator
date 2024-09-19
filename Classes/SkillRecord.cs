83cfdd34c3c2f66c345616ba0c3e6f2003fe1c26
Hunerss <107001861+Hunerss@users.noreply.github.com>
2024-09-19 22:50:05 +0200

    Adds class for string stat points

---
+++ Classes/SkillRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CyberpunkRedCharacterCreator.Classes
+{
+    public class SkillRecord
+    {
+        public int Roll { get; set; }
+        public int INT { get; set; }
+        public int REF { get; set; }
+        public int DEX { get; set; }
+        public int TECH { get; set; }
+        public int COOL { get; set; }
+        public int WILL { get; set; }
+        public int LUCK { get; set; }
+        public int MOVE { get; set; }
+        public int BODY { get; set; }
+        public int EMP { get; set; }
+    }
+}
